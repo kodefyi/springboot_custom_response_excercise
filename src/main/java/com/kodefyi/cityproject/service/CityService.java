40ab173105d1626ee7bb2e6796dfa684dbcb50b0
devil730 <a30sham.07@gmail.com>
2022-11-03 21:08:53 +0530

    custom JSON response

---
+++ src/main/java/com/kodefyi/cityproject/service/CityService.java
@@ -0,0 +1,15 @@
+package com.kodefyi.cityproject.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kodefyi.cityproject.entity.City;
+
+@Service
+public interface CityService {
+
+	List<City> addCity(List<City> city);
+
+	List<City> getCityWithPincodeRange(long start, long end);
+}
